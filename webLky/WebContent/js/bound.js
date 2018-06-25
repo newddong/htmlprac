286ab23be7b03516be5960203a275137de521c48
newdd <newdd@DESKTOP-CLJ9RM4>
2018-06-26 02:36:00 +0900

    interval study

---
+++ webLky/WebContent/js/bound.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+
+window.onload = function(){
+	
+	var box = document.getElementById("box");
+	
+	box.style.backgroundColor = "green";
+	box.style.width = "100px";
+	box.style.height = "100px";
+	box.style.position = "relative";
+	box.style.left = "0px"; 
+		
+	box.addEventListener("click", function(){
+		box.style.left = parseInt(box.style.left)+1+"px";
+	})
+	
+
+	box.on
+	
+	
+	
+	
+	
+		
+	
+}
--- webLky/WebContent/js/js1.js
+++ webLky/WebContent/js/js1.js
@@ -11,11 +11,16 @@ window.onload = function() {
 	box1.style.position = "absolute";
 	box1.style.top = "100px";
 	
-
+	var p = document.createElement("p");
+	var t = document.createTextNode("");
+	t.textContent = "0";
+	document.body.appendChild(p);
+	p.appendChild(t);
+	
 	box1.addEventListener("click",function(){
 		box1.children[0].textContent = parseInt(box1.style.top)+100+"px";
 		box1.style.backgroundColor = "green";
-		box1.style.top += parseInt(box1.style.top)+100+"px";
+		box1.style.top = parseInt(box1.style.top)+10+"px";
 		box1.children[0].textContent  = box1.style.top;
 	} )
 	
@@ -28,6 +33,19 @@ window.onload = function() {
 		box2.style.top = "200px";
 		document.body.appendChild(box2);
 	})
-
+	box1.addEventListener("click",function(){
+		var p = document.getElementById("edit");
+		p.style.color = "red";
+		p.textContent = parseInt(p.textContent)+1;
+		
+	})
+	
+	document.body.addEventListener("mousemove", function(e) {
+		t.textContent = "x:"+e.clientX + "y:"+e.clientY;
+		
+		
+	})
+	
 
 }
+
