c5f931d77e028abea01ab341c55e8dd4ac52d1ad
newdd <newdd@DESKTOP-CLJ9RM4>
2018-06-29 02:56:39 +0900

    123

--- webLky/WebContent/js/practice.js
+++ webLky/WebContent/js/practice.js
@@ -7,21 +7,28 @@ window.onload = function(){
 	
 	var list = document.getElementsByClassName("menu2")[0].children[0];
 	
+	for(var i=0;i<list.children.length;i++){
+	
+	(function(j){
+	list.children[j].addEventListener("mouseover", function(){
+	list.children[j].className = "select";
+	list.children[j].children[0].style.display = "unset";
+	list.children[j].style.color = "#1c1c1c";
+	list.children[j].style.fontWeight = "bold";
+	
+	})
+	
+	list.children[j].addEventListener("mouseout", function(){
+	list.children[j].removeAttribute("class");
+	list.children[j].children[0].style.display = "none";
+	list.children[j].style.color = "#777";
+	list.children[j].style.fontWeight = "normal";
+	})})(i);
 	
 	
-	list.children[0].addEventListener("mouseover", function(e) {
-		list.children[0].children[0].style.display = "unset";
-	});
-	
-	list.children[0].addEventListener("mouseout", function(e) {
-		list.children[0].children[0].style.display = "none";
-	});
-	
-	list.children[0].style.background = "blue";
+	}
 	
 	
-//	var k = document.createElement("style");
-//	k.innerHTML = ".head .menubar .menu2>ul>li:after{display:none;}";
-//	document.body.appendChild(k);
+	
 	
 }
