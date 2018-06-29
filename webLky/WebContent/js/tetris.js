9bb2b9e10de5c5e3bb8ccba142c6a4f9462d3ec5
newdd <newdd@DESKTOP-CLJ9RM4>
2018-06-29 13:16:12 +0900

    BOARD GENERATION

--- webLky/WebContent/js/tetris.js
+++ webLky/WebContent/js/tetris.js
@@ -3,21 +3,36 @@
  */
 
 $(function(){
+	var BOARD_HEIGHT=10;
+	var BOARD_WIDTH=10;
+	
+	var board = new Array();
 
-	
-	$(".gamepanel").html("<div></div>");
-	$(".gamepanel").append("<div></div>");
-	$(".gamepanel").children().addClass("block1");
-	$(".gamepanel").children().addClass("block2");
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	function boardsetting(BOARD_WIDTH, BOARD_HEIGHT) {
+		for (var i = 0; i < BOARD_WIDTH; i++) {
+			for (var j = 0; j < BOARD_HEIGHT; j++) {
+				board[i][j] = 0;
+			}
+		}
+
+		for (var i = 0; i < BOARD_WIDTH + 1; i++) {
+			board[i][BOARD_HEIGHT + 1] = 1;
+		}
+
+		for (var i = 0; i < BOARD_HEIGHT + 1; i++) {
+			board[BOARD_WIDTH + 1][i] = 1;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
 })
 
 
